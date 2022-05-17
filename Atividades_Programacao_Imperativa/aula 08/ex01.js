564794952e42970268157e1adb110fb1dee91198
DronineOnlyOne <matheu.radical@gmail.com>
2022-05-17 20:14:27 -0300

    Atividade_Aula_08_Program_Impertativa

--- Atividades_Programacao_Imperativa/aula 08/ex01.js
+++ Atividades_Programacao_Imperativa/aula 08/ex01.js
@@ -1,23 +1,26 @@
 let acompanhada = true
-let altura = ''
+let altura = ""
+
 function podeSubir (altura, acompanhada) {
     let podeSubir = false
 
-    if (altura >= 1.40 || altura == 2.0) {
-        podeSubir = true
-        console.log("Acesso Autorizado")
-    }
-    if (altura > 1.20 || altura == 1.40 && acompanhada) {
-        podeSubir = true 
-        console.log ("Acesso Autorizado com acompanhante")
+    if (altura >= 1.40 && altura <= 2.0) {
+        return console.log("Acesso Autorizado")
     }
 
-    if (altura < 1.20 || acompanhada !== acompanhada) {
-        console.log ("Acesso negado")
+    else if (altura > 1.20 && altura <= 1.40 && acompanhada) {
+        return console.log ("Acesso Autorizado com acompanhante")
     }
 
-    return podeSubir
+    else if (altura < 1.20) {
+        return console.log ("Acesso negado")
+    }
+
+    else {
+       return console.log ("Acesso negado")
+    }
 }
 
+podeSubir (1.35, acompanhada)
 
-console.log (podeSubir (1.15, acompanhada))
+// Equipe: Matheus Ribeiro, Igor Avelar, Filipe Silva, Michel Vieira
