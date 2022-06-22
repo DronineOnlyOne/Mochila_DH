39e608252d6ee6975bad14a06f3d694f9454a0f2
DronineOnlyOne <matheu.radical@gmail.com>
2022-06-22 19:00:56 -0300

    Atualizacao de Atividades 3

--- Atividades_Programacao_Imperativa/aula 18/ex01.js
+++ Atividades_Programacao_Imperativa/aula 18/ex01.js
@@ -1,46 +1,46 @@
 
 //Loop de Pares
 
-console.log (" -------------------------- Loop de Pares ------------------------- ")
+console.log(" -------------------------- Loop de Pares ------------------------- ")
 
-function loopDePares (a) {
+function loopDePares(a) {
     for (let i = 0; i <= 100; i++) {
-        console.log((a + i) % 2 === 0 ? 'O número '+ i +' é par' : (a + i)); 
+        console.log((a + i) % 2 === 0 ? 'O número ' + i + ' é par' : (a + i));
     }
 }
 
-loopDePares (6) 
+loopDePares(6)
 
 // Loop ímpares com palavra
 
-console.log (" -------------------------- Loop de Impares ------------------------- ")
+console.log(" -------------------------- Loop de Impares ------------------------- ")
 
-function loopDeImpares (a) {
+function loopDeImpares(a) {
     for (let i = 0; i <= 100; i++) {
-        console.log((a + i) % 2 === 1 ? 'O número '+ i +' é impar' : (a + i)); 
+        console.log((a + i) % 2 === 1 ? 'O número ' + i + ' é impar' : (a + i));
     }
 }
 
-loopDeImpares (6) 
+loopDeImpares(6)
 
 // Soma 
 
-console.log (" -------------------------- Soma ------------------------- ")
+console.log(" -------------------------- Soma ------------------------- ")
 
-function soma (valor) {
+function soma(valor) {
     if (valor > 0) {
-    valor = valor + soma(valor - 1)
+        valor = valor + soma(valor - 1)
     }
     return valor
 }
 
-console.log (soma(5))
+console.log(soma(5))
 
 // Novo Array
- 
-console.log (" -------------------------- Novo Array ------------------------- ")
 
-function newArray (valor) {
+console.log(" -------------------------- Novo Array ------------------------- ")
+
+function newArray(valor) {
     let array = [];
     for (let i = 1; i <= valor; i++) {
         array.push(i)
@@ -48,7 +48,7 @@ function newArray (valor) {
     return console.log(valor)
 }
 
-function newArray2 (valor) {
+function newArray2(valor) {
     let array2 = [];
     for (let i = 1; i <= valor; i++) {
         array2.push(i)
@@ -61,9 +61,9 @@ newArray(5)
 
 //String split
 
-console.log (" -------------------------- String Split ------------------------- ")
+console.log(" -------------------------- String Split ------------------------- ")
 
-function split (a) {
+function split(a) {
     let array = [];
     for (let i = 0; i < a.length; i++) {
         array.push(a[i])
@@ -75,9 +75,62 @@ split("DigitalHouse")
 
 // Zero pro final 
 
-console.log (" -------------------------- Zero pro final ------------------------- ")
+console.log(" -------------------------- Zero pro final ------------------------- ")
 
-function moverZeros(a){
-    array: [0,0,0,0,0,0,0,1,3,4,5,6]
-    
+const array0 = [0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6]
+
+
+function moverZeros(array0) {
+    let zeros = []
+    let semZeros = []
+
+    array0.filter(element => element == 0 ? zeros.push(element) : semZeros.push(element))
+    return semZeros.concat(zeros)
 }
+
+console.log(moverZeros(array0))
+
+
+console.log(" --------------------- Lidando com dois arrays ----------------------- ")
+
+
+const arrayNumero = [1, 2, 3]
+const arrayPalavra = ['O', 'L', 'A']
+
+function arrayHandler(a, b) {
+
+    for (let i = 0; i < a.length || i < b.length; i++) {
+        console.log("Eu sou " + a[i] + " e eu sou " + b[i])
+    }
+}
+
+arrayHandler(arrayNumero, arrayPalavra)
+
+
+console.log(" --------------------- Arrays de objetos ----------------------- ")
+
+function arrayObjetos(a) {
+    objetos = []
+    for (let i = 1; i <= a; i++) {
+        objetos.push({Valor: i})
+    }
+
+    return console.log(objetos)
+}
+
+arrayObjetos(5)
+
+
+console.log(" --------------------- Arrays de objetos 2 ----------------------- ")
+
+function arrayObjetosDois (a,b){
+    objetosDois = []
+    for (let i = 1; i <= a; i++) {
+        objetosDois.push ({[b] : i})
+        
+    }
+
+    return console.log(objetosDois)
+}
+
+arrayObjetosDois (5,"Acabou")
